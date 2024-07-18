160352f5ae4e447c9af89dee22277d69502754f2
Eugene <eugene.kasap@gmail.com>
2024-07-18 16:44:47 +0200

    Create Get Customer by Id Endpoint

--- apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/controllers/CustomerController.java
+++ apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/controllers/CustomerController.java
@@ -5,10 +5,12 @@ import org.emerald.apifirst.apifirstserver.services.CustomerService;
 import org.emerald.apifirst.model.Customer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.emerald.apifirst.apifirstserver.controllers.CustomerController.BASE_URL;
 
@@ -24,5 +26,10 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<Customer>> listCustomers() {
         return ResponseEntity.ok(customerService.listCustomers());
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
     }
 }
--- apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/services/CustomerService.java
+++ apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/services/CustomerService.java
@@ -3,7 +3,10 @@ package org.emerald.apifirst.apifirstserver.services;
 import org.emerald.apifirst.model.Customer;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<Customer> listCustomers();
+
+    Customer getCustomerById(UUID customerId);
 }
