bfd4dedae420b04dbe87b92c165e50716df73b47
Eugene <eugene.kasap@gmail.com>
2024-07-28 22:09:24 +0200

    Create customer API endpoint

--- apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/controllers/CustomerController.java
+++ apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/controllers/CustomerController.java
@@ -6,9 +6,12 @@ import org.emerald.apifirst.model.Customer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,5 +34,11 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") UUID customerId) {
         return ResponseEntity.ok(customerService.getCustomerById(customerId));
+    }
+
+    @PostMapping
+    ResponseEntity<Void> saveNewCustomer(@RequestBody Customer customer){
+        var savedCustomer = customerService.saveNewCustomer(customer);
+        return ResponseEntity.created(URI.create(BASE_URL + "/" + savedCustomer.getId())).build();
     }
 }
--- apifirst-server/src/test/java/org/emerald/apifirst/apifirstserver/controllers/CustomerControllerTest.java
+++ apifirst-server/src/test/java/org/emerald/apifirst/apifirstserver/controllers/CustomerControllerTest.java
@@ -1,5 +1,8 @@
 package org.emerald.apifirst.apifirstserver.controllers;
 
+import org.emerald.apifirst.model.Address;
+import org.emerald.apifirst.model.Customer;
+import org.emerald.apifirst.model.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +10,8 @@ import org.springframework.http.MediaType;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +35,37 @@ public class CustomerControllerTest extends BaseTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(testCustomer.getId().toString()));
+    }
+
+    @DisplayName("Test Create Customer")
+    @Test
+    void testCreateCustomer() throws Exception {
+        var customer = Customer.builder()
+                .name(Name.builder()
+                        .lastName("Doe")
+                        .firstName("John")
+                        .build())
+                .phone("555-555-5555")
+                .email("john@example.com")
+                .shipToAddress(Address.builder()
+                        .addressLine1("123 Main St")
+                        .city("Denver")
+                        .state("CO")
+                        .zip("80216")
+                        .build())
+                .billToAddress(Address.builder()
+                        .addressLine1("123 Main St")
+                        .city("Denver")
+                        .state("CO")
+                        .zip("80216")
+                        .build())
+                .build();
+
+        mockMvc.perform(post(CustomerController.BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
     }
 
 }
