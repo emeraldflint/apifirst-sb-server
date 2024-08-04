8cf59c6896f10482c398d32063b32534314ddd56
Eugene <eugene.kasap@gmail.com>
2024-08-04 13:58:21 +0200

    Create Order Update Mapper

--- apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/mappers/OrderMapper.java
+++ apifirst-server/src/main/java/org/emerald/apifirst/apifirstserver/mappers/OrderMapper.java
@@ -3,13 +3,29 @@ package org.emerald.apifirst.apifirstserver.mappers;
 import org.emerald.apifirst.apifirstserver.domain.Order;
 import org.emerald.apifirst.model.OrderCreateDto;
 import org.emerald.apifirst.model.OrderDto;
+import org.emerald.apifirst.model.OrderUpdateDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper
 @DecoratedWith(OrderMapperDecorator.class)
 public interface OrderMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "customer", ignore = true)
+    @Mapping(target = "shipmentInfo", ignore = true)
+    @Mapping(target = "orderStatus", ignore = true)
+    @Mapping(target = "selectedPaymentMethod", ignore = true)
+    @Mapping(target = "orderLines", ignore = true)
+    @Mapping(target = "dateUpdated", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    void updateOrder(OrderUpdateDto orderDto, @MappingTarget Order order);
+
+    @Mapping(target = "selectPaymentMethodId", ignore = true)
+    @Mapping(target = "customerId", ignore = true)
+    OrderUpdateDto orderToUpdateDto(Order order);
+
     @Mapping(target = "shipmentInfo", ignore = true)
     @Mapping(target = "selectedPaymentMethod", ignore = true)
     @Mapping(target = "orderStatus", ignore = true)
